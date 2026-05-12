6e95e0808f2542ef2e5683faf99cf10cabbf3803
r-follador <r.follador@gmail.com>
2026-05-12 21:06:58 +0200

    upgrade to java 25

--- src/test/java/com/genewarrior/genewarrior/GeneWarriorApplicationTests.java
+++ src/test/java/com/genewarrior/genewarrior/GeneWarriorApplicationTests.java
@@ -3,7 +3,14 @@ package com.genewarrior.genewarrior;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:genewarrior;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;DB_CLOSE_ON_EXIT=FALSE",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "logging.level.root=INFO"
+})
 class GeneWarriorApplicationTests {
 
     @Test
